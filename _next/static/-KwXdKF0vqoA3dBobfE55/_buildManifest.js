724a60bae9825ea1c298191c863e5cf9c6247bd7
Jiecai <jiecai@stu.pku.edu.cn>
2025-11-26 11:52:28 +0800

    Updates

--- _next/static/-KwXdKF0vqoA3dBobfE55/_buildManifest.js
+++ _next/static/-KwXdKF0vqoA3dBobfE55/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/hnu-hedp//_next/:path+",
+        "destination": "/hnu-hedp/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
